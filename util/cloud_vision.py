8ee1c9e83577d1b27ab7160b349d8cedad1e080e
rhdzmota <rhdzmota@mxquants.com>
2017-10-01 20:41:20 -0500

    standalone OCR, probably not working.

--- util/cloud_vision.py
+++ util/cloud_vision.py
@@ -1,30 +1,13 @@
+from util.dropbox_api import DropBoxFileManager
 from settings import config
-from dropbox_api import DropBoxFileManager
-import base64
-import json
 import requests
-#import requests_toolbelt.adapters.appengine
+import json
 
-#requests_toolbelt.adapters.appengine.monkeypatch()
+
 VISION_URL = "https://vision.googleapis.com/v1/images:annotate?key=" + config.VISION_KEY
 
 
 class OCR(object):
-
-    @staticmethod
-    def get_text_from_file(file_name):
-        with open(file_name, "rb") as file:
-            contents = file.read()
-        return OCR.get_text_from_bytes(contents)
-
-    @staticmethod
-    def get_text_from_bytes(file_bytes):
-        r = requests.post(
-            url=VISION_URL,
-            data=OCR.base64_request_data(file_bytes),
-            headers={'content-type': 'application/json'}
-        )
-        return OCR.extract_text(r)
 
     @staticmethod
     def get_text_from_url(file_url, user="temp"):
@@ -42,25 +25,6 @@ class OCR(object):
     def extract_text(response):
         response.raise_for_status()
         return str(response.json()["responses"][0]["fullTextAnnotation"]['text'])
-
-    @staticmethod
-    def base64_request_data(file_bytes):
-        return json.dumps(
-            {
-                "requests": [
-                    {
-                        "image": {
-                            "content": base64.b64encode(file_bytes).decode("utf-8")
-                        },
-                        "features": [
-                            {
-                                "type": "TEXT_DETECTION"
-                            }
-                        ]
-                    }
-                ]
-            }
-        )
 
     @staticmethod
     def url_request_data(file_url):
